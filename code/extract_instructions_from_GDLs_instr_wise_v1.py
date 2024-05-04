f95d7546bd30f6e7a574b0e7e4bbbc78c3764421
ifty58 <ifty58@gmail.com>
2024-05-04 15:17:38 -0400

    To convert GDLs to one-instruction-per-line format
    Convert GDLs to one-instruction-per-line format by running below command for the program extract_instructions_from_GDLs_instr_wise_v1.py. 
    Do this for both x86 and ARM GDLs.
    
    python3 extract_instructions_from_GDLs_instr_wise_v1.py --input_path INPUT_FOLDER_WITH_PREPROCESSED_GDLS --output_path OUTPUT_FOLDER_FOR_CONVERTED_GDLs

---
+++ code/extract_instructions_from_GDLs_instr_wise_v1.py
@@ -0,0 +1,47 @@
+import argparse
+import os, time
+
+start_time = time.time()
+
+parser = argparse.ArgumentParser(description='Convert GDLs to one-instruction-per-line format')
+parser.add_argument('--input_path', help='input folder with preprocessed GDLs')
+parser.add_argument('--output_path', help='output folder for converted GDLs')
+args = parser.parse_args()
+
+GDL_FOLDER_PATH = args.input_path
+OUTPUT_GDL_FOLDER_PATH = args.output_path
+
+file_names = os.listdir(GDL_FOLDER_PATH)
+for file in file_names:
+    inst_strings = ''
+    filepath = os.path.join(GDL_FOLDER_PATH, file)
+    output_filepath = os.path.join(OUTPUT_GDL_FOLDER_PATH, file)
+    with open(filepath, "r", errors='ignore') as fp_gdl_in:
+        for gdl_line in fp_gdl_in:
+            gdl_line = gdl_line.strip()
+            if gdl_line.startswith('node: {'):
+                continue
+            elif gdl_line.startswith('{'):
+                continue
+            elif gdl_line.startswith('}'):
+                continue
+            elif gdl_line.startswith('edge: {'):
+                continue
+            elif gdl_line == '\n':
+                continue
+            elif gdl_line == '0}':
+                continue
+            elif gdl_line == '0':
+                continue
+            elif len(gdl_line.strip()) == 0:
+                continue
+            else:
+                gdl_line = gdl_line.replace(' ', '\n')
+                inst_strings = inst_strings + gdl_line + '\n'
+        inst_strings = inst_strings.strip()
+        fp_gdl_in.close()
+    with open(output_filepath, "w") as fp_out:
+        fp_out.write(inst_strings)
+        fp_out.close()
+execution_time = time.time() - start_time
+print("--- {} seconds ---".format(execution_time))
